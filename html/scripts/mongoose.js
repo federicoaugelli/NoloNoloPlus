fb5fd206c8b5290be89e0642af1fae50bbdf9f84
ma3ti <matteo.fontana9@libero.it>
2022-01-02 17:27:23 +0100

    a
    a

--- html/scripts/mongoose.js
+++ html/scripts/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/site202127', {useNewUrlParser: true, useUnifiedTopology: true});
+//const mongouri = `mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`;
+
+
+//mongoose.connect('mongodb://127.0.0.1:27017/site202127', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 var conn = mongoose.connection;
 conn.on('connected', function() {
     console.log('MONGOOSE database is connected successfully');
