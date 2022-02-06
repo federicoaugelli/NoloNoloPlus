89616b50eb35fe5b53d1e42e708d9f951c50d720
ma3ti <matteo.fontana9@libero.it>
2022-02-06 19:51:04 +0100

    a
    a

--- html/scripts/mongoose.js
+++ html/scripts/mongoose.js
@@ -1,20 +1,16 @@
 var mongoose = require('mongoose');
 
-//const mongouri = `mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`;
-
-/*
 const credentials = {
 	user: "site202127",
 	pwd: "eer6Beir",
 	site: "mongo_site202127"
 } 
-*/
 
-mongoose.connect('mongodb://127.0.0.1:27017/site202127', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//mongoose.connect('mongodb://127.0.0.1:27017/site202127', {useNewUrlParser: true, useUnifiedTopology: true});
 //mongoose.connect(`mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 
-//const mongouri = `mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`;
-
+/*
 var conn = mongoose.connection;
 conn.on('connected', function() {
     console.log('MONGOOSE database is connected successfully');
@@ -23,7 +19,26 @@ conn.on('disconnected',function(){
     console.log('database is disconnected successfully');
 })
 conn.on('error', console.error.bind(console, 'connection error:'));
+*/
+
+/*
+main().catch(err => console.log(err));
+
+async function main() {
+	await mongoose.connect(`mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+  }
+*/
+//module.exports = mongoose.connection;
 
 
+mongoose.connect(`mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`,{
+    useNewUrlParser: false
+},err=>{
+    if(err){
+        console.error(err)
+    }else {
+        console.log ('mongodb successfully connected ')
+    }
+})
 
-module.exports = conn;
+module.exports = mongoose.connect;
